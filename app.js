782f4c6e6b77da33f44599f14a8a3c794be6cca9
unknown <pablo.hinojosa@ort.edu.ar>
2021-04-09 18:05:35 -0300

    spread y ejercicio tipo examen

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-console.log("Hola mundo!");
+const chalk = require('chalk');
+
+console.log(chalk.magenta("Hola mundo!"));
 console.log([16,5,6,7,8].sort((a,b)=> a -b));
 
 [45,55,4,1].forEach(num => console.log(num));
